ef10c6b57aecf44a1e4c2dc4b3f3e3c1f433025c
Fernando <fernandorato.0@hotmail.com>
2020-02-18 16:51:24 -0300

    Middlewares Recipients

---
+++ Backend/src/app/Controller/RecipientsController.js
@@ -0,0 +1,11 @@
+class RecipientsController {
+    async store(req, res) { }
+
+    async index(req, res) { }
+
+    async show(req, res) { }
+
+    async delete(req, res) { }
+}
+
+export default RecipientsController;
