328a211e9ff6afb6023386e6bc05e4110c23dbcb
Fernando <fernandorato.0@hotmail.com>
2020-02-18 16:37:32 -0300

    criado middleware de autenticação, criado update de usuario

--- Backend/src/app/Controller/UserController.js
+++ Backend/src/app/Controller/UserController.js
@@ -35,6 +35,54 @@ class UserController {
             return res.status(err.status).send(err);
         }
     }
+
+    async update(req, res) {
+        const schema = Yup.object().shape({
+            name: Yup.string().required(),
+            email: Yup.string()
+                .email()
+                .required(),
+            oldPassword: Yup.string().min(6),
+            password: Yup.string()
+                .min(6)
+                .when('oldPassword', (oldPassword, field) =>
+                    oldPassword ? field.required() : field
+                ),
+            confirmPassword: Yup.string().when('password', (password, field) =>
+                password ? field.required().oneOf([Yup.ref('password')]) : field
+            ),
+        });
+
+        if (!(await schema.isValid(req.body))) {
+            return res.status(401).json({ error: 'Validation Fails' });
+        }
+
+        try {
+            const { email, oldPassword } = req.body;
+            console.log(req.userId);
+            const user = await User.findByPk(req.userId);
+
+            if (user.email !== email) {
+                const emailExist = User.findOne({
+                    where: { email },
+                });
+                if (emailExist)
+                    return res
+                        .status(401)
+                        .json({ Error: 'Email already in use' });
+            }
+
+            if (oldPassword && !(await user.checkPassword(oldPassword))) {
+                return res.status(401).json({ error: 'Password Incorrect' });
+            }
+
+            const response = await user.update(req.body);
+
+            return res.json({ response });
+        } catch (err) {
+            return res.status(401).json({ error: 'Update Failured' });
+        }
+    }
 }
 
 export default new UserController();
--- Backend/src/routes.js
+++ Backend/src/routes.js
@@ -3,6 +3,8 @@ import { Router } from 'express';
 import SessionController from './app/Controller/SessionController';
 import UserController from './app/Controller/UserController';
 
+import authMiddlewares from './app/middlewares/auth';
+
 const routes = new Router();
 
 // login
@@ -10,5 +12,10 @@ routes.post('/sessions', SessionController.store);
 
 // Create Users
 routes.post('/users', UserController.store);
+
+// Routes with authenticate
+routes.use(authMiddlewares);
+
+routes.put('/users/', UserController.update);
 
 export default routes;
