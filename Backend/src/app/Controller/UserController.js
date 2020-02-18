34900bbbf786d589372e85002a2ad65a0cfc95b2
Fernando <fernandorato.0@hotmail.com>
2020-02-18 12:43:41 -0300

    Create users ok

--- Backend/src/app/Controller/UserController.js
+++ Backend/src/app/Controller/UserController.js
@@ -1,15 +1,40 @@
+import * as Yup from 'yup';
 import User from '../Models/user';
 
 class UserController {
-    async store(req, res) { }
+    async store(req, res) {
+        const schema = Yup.object().shape({
+            name: Yup.string().required(),
+            email: Yup.string()
+                .email()
+                .required(),
+            password: Yup.string()
+                .required()
+                .min(6),
+        });
 
-    async update(req, res) { }
+        if (!(await schema.isValid(req.body))) {
+            return res.status(400).json({ Error: ' Validation Fails' });
+        }
 
-    async show(req, res) { }
+        try {
+            const emailExist = await User.findOne({
+                where: { email: req.body.email },
+            });
 
-    async index(req, res) { }
+            if (emailExist) {
+                return res.status(400).json({ error: 'User already exist' });
+            }
 
-    async delete(req, res) { }
+            const response = await User.create(req.body);
+
+            const { id, name, email, createdAt } = response;
+
+            return res.json({ id, name, email, createdAt });
+        } catch (err) {
+            return res.status(err.status).send(err);
+        }
+    }
 }
 
 export default new UserController();
--- Backend/src/routes.js
+++ Backend/src/routes.js
@@ -1,9 +1,14 @@
 import { Router } from 'express';
 
 import SessionController from './app/Controller/SessionController';
+import UserController from './app/Controller/UserController';
 
 const routes = new Router();
 
+// login
 routes.post('/sessions', SessionController.store);
+
+// Create Users
+routes.post('/users', UserController.store);
 
 export default routes;
