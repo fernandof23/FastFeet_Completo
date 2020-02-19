bf2055108052087c8bec4e514e4f1cbf3e2f5163
Fernando <fernandorato.0@hotmail.com>
2020-02-19 16:21:06 -0300

    Criado relacionamento Deliverymans com Avatar

--- Backend/src/database/index.js
+++ Backend/src/database/index.js
@@ -16,7 +16,12 @@ class Database {
     init() {
         this.connection = new Sequelize(dataBaseConfig);
 
-        models.map(model => model.init(this.connection));
+        models
+            .map(model => model.init(this.connection))
+            .map(
+                model =>
+                    model.associate && model.associate(this.connection.models)
+            );
     }
 }
 
